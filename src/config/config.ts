a0716b7bc50d25ee4e0afd829cc784ec1c2c5a9b
GHOST-Aram <owinoojala@gmail.com>
2024-02-29 22:16:44 +0300

    refactor: Handle DB connection string error
             - Check if the connection string is available before attempting to connect
             - Throw error if the connection string is not available

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 import express, { Application } from "express"
 import { authenticator } from "../z-library/auth/auth"
 import { Server } from "../z-library/server/server"
-import 'dotenv/config'
 import { Connection } from "../z-library/db/connection"
+import 'dotenv/config'
 
 const app: Application = express()
 const server = new Server(app)
@@ -12,24 +12,31 @@ server.allowCrossOriginResourceSharing()
 server.enforceSecurity()
 server.logRequestsandResponses()
 
-const dbUri = process.env.USERSDB_URI || ''
+const dbUri = process.env.USERSDB_URI
 const secretOrKey = process.env.TOKEN_SECRET
 let connection: Connection
 
 try {
     
-    connection = new Connection(dbUri)
-    
-    const authDbConnection = connection.getInitial()
-    
-    if(secretOrKey){
-        authenticator.configureStrategy(secretOrKey, authDbConnection)
-        authenticator.initialize(app)
-    } else {
-        console.log(
-            'Authentication Secret Key is Undefined. '
-            +'Please provide all of them in enviroment variables')
+    if(dbUri){
+        console.log('DB URI: ', dbUri)
+        connection = new Connection(dbUri)
+        
+        const authDbConnection = connection.getInitial()
+        
+        if(secretOrKey){
+            authenticator.configureStrategy(secretOrKey, authDbConnection)
+            authenticator.initialize(app)
+        } else {
+           throw new Error(
+                'Authentication Secret Key is Undefined. '
+                +'Please provide all of them in enviroment variables')
+        }
     }
+     else {
+        throw new Error('Database Connection String not Found')
+    }
+   
 } catch (error: any) {
     console.log(error.message)
 }
