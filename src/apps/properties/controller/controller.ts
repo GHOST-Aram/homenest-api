e5f99595fa9acffe1b40883aae66c8041b75137e
GHOST-Aram <owinoojala@gmail.com>
2024-03-08 08:59:17 +0300

    refactor: Override user input for properties agentId
             - Override GenericeController.addNew Method in properties controller
             - Override the value of agentId in the user input
             - Use the id of currently logged in user instead of the id provided by client

--- src/apps/properties/controller/controller.ts
+++ src/apps/properties/controller/controller.ts
@@ -1,8 +1,24 @@
 import { RentalDataAccess } from "../data-access/data-access";
 import { GenericController } from "../../../z-library/bases/generic-controller";
+import { Request, Response, NextFunction } from "express";
 
 export class RentalsController extends GenericController<RentalDataAccess>{
     constructor (dataAccess: RentalDataAccess, microserviceName: string){
         super(dataAccess, microserviceName)
+    }
+
+    public addNew = async(req: Request, res: Response, next: NextFunction) =>{
+        const inputData = req.body
+        const currentUser:any = req.user
+
+        try {
+            const newDocument = await this.dataAccess.createNew({
+                ...inputData, agentId: currentUser._id.toString()
+            })
+            
+            this.respondWithCreatedResource(newDocument, res)
+        } catch (error) {
+            next(error)
+        }   
     }
 }
