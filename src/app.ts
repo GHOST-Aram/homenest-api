d8ade79043559c484f9a17c09d0cbf65f908b37c
GHOST-Aram <owinoojala@gmail.com>
2024-08-15 10:01:56 +0300

    refator: Use the object of Zero class from zero lib to configure system URLS
             - Create a URLS metadata array and use zero.configureUrls method to
               configure URLs

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,33 @@
 import { authRoutes } from "./apps/authenticator/app";
-import { app } from "./_config/config";
+import { zero } from "./zero/zero";
 import { galleryRoutes } from "./apps/gallery/app";
-import { httpErrors } from "./z-library/HTTP/http-errors";
 import { usersRouter } from "./apps/users/app";
 import { propertyRoutes } from "./apps/properties/app";
 import { messagesRoutes } from "./apps/messenger/app";
 import { reviewsRoutes } from "./apps/reviews/app";
-import { Response, Request } from "express";
 import { routes as viewingScheduleRoutes } from "./apps/viewing-scheduler/app";
 import { routes as applicationsRoutes } from "./apps/rental-applications/app";
+import { URLMetadata } from "./zero/zero/types";
 
-app.get('/', (req: Request, res: Response) =>{
-    res.status(200).json({ message: "Welcome to Homenest API" })
-})
-app.use('/auth', authRoutes)
-app.use('/gallery', galleryRoutes)
-app.use('/users', usersRouter)
-app.use('/properties', propertyRoutes)
-app.use('/messages', messagesRoutes)
-app.use('/reviews', reviewsRoutes)
-app.use('/viewing-scheduler', viewingScheduleRoutes)
-app.use('/rental-applications', applicationsRoutes)
+const urlsData: URLMetadata[] = [
 
+    { path: '/auth', router: authRoutes },
+    { path: '/gallery', router: galleryRoutes },
+    { path: '/users', router: usersRouter },
+    { path: '/properties', router: propertyRoutes },
+    { path: '/messages', router: messagesRoutes },
+    { path: '/reviews', router: reviewsRoutes },
+    { path: '/viewing-scheduler', router: viewingScheduleRoutes },
+    { path: '/rental-applications', router: applicationsRoutes },
+]
+
+
+try {
+    zero.configureUrls(urlsData)
+    
+} catch (error:any) {
+    console.warn("Error occured while configuring urls: ", error.message)
+}
 //Handle errors -- Unknown path
-app.use(httpErrors.handleUnknownUrls)
-app.use(httpErrors.handleServerErrors)
+zero.handleUnknownUrls()
+zero.handleServerErrors()
